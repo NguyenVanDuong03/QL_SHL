63eca16bcb5a11b586e07b684eda2a45212ebfca
DuongNV <19.nguyenvanduong@gmail.com>
2025-05-29 14:35:15 +0700

    update language vi

---
+++ lang/vi/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dòng Ngôn Ngữ Xác Thực
+    |--------------------------------------------------------------------------
+    |
+    | Các dòng ngôn ngữ sau được sử dụng trong quá trình xác thực để hiển thị
+    | các thông báo đến người dùng. Bạn có thể thay đổi nội dung theo nhu cầu
+    | của ứng dụng của bạn.
+    |
+    */
+
+    'failed' => 'Thông tin đăng nhập không đúng.',
+    'password' => 'Mật khẩu không chính xác.',
+    'throttle' => 'Đăng nhập quá nhiều lần. Vui lòng thử lại sau :seconds giây.',
+
+];
---
+++ lang/vi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dòng Ngôn Ngữ Đặt Lại Mật Khẩu
+    |--------------------------------------------------------------------------
+    |
+    | Các dòng ngôn ngữ sau là mặc định, phản ánh các lý do có thể xảy ra
+    | khi cố gắng cập nhật mật khẩu, ví dụ như token không hợp lệ
+    | hoặc mật khẩu không thỏa mãn điều kiện.
+    |
+    */
+
+    'reset' => 'Mật khẩu của bạn đã được đặt lại.',
+    'sent' => 'Chúng tôi đã gửi liên kết đặt lại mật khẩu đến email của bạn.',
+    'throttled' => 'Vui lòng chờ trước khi thử lại.',
+    'token' => 'Token đặt lại mật khẩu không hợp lệ.',
+    'user' => 'Không tìm thấy người dùng với địa chỉ email này.',
+
+];
